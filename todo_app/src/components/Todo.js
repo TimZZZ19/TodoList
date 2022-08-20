ccd9935532ca75f783431b38a50fbb2650f35564
TimZ <tiomsity@gmail.com>
2022-08-20 14:54:46 -0500

    changed the look of todo

--- todo_app/src/components/Todo.js
+++ todo_app/src/components/Todo.js
@@ -4,10 +4,10 @@ import styles from "./Todo.module.css";
 const Todo = ({ todo }) => {
   return (
     <li className={styles.todo}>
-      <span className={styles["todo-name"]}>{todo}</span>
       <button className={styles["check-btn"]} type="button">
         <i className="fas fa-check"></i>
       </button>
+      <span className={styles["todo-name"]}>{todo}</span>
       <button className={styles["delete-btn"]} type="button">
         <i className="fas fa-trash"></i>
       </button>
